80cc5669eabd4931defbc5dd3163e7d22e4f50b9
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-24 16:50:32 +0100

    improve jumbo component

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="my-container d-flex">
-        <div class="description-container d-flex flex-column justify-content-center align-items-start ms-5">
+    <div class="jumbo-container d-flex position-relative">
+        <!-- <div class="description-container d-flex flex-column justify-content-center align-items-start ms-5">
             <p> Always the best way you need it</p>
             <h1>THE BEST BUSINESS CONSULTING</h1>
             <div><button>READ MORE</button><i class="fas fa-play"></i></div>
+        </div> -->
+        <div class="jumbo-description float-start positione-relative">
+            <div class="description-container ms-5">
+            <p> Always the best way you need it</p>
+            <h1>THE BEST BUSINESS CONSULTING</h1>
+            <div><button class="me-3">READ MORE</button><i class="fas fa-play"></i></div>
+        </div>
+        </div>
+        <div class="jumbo-img float-end">
+            <img src="../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg" alt="">
         </div>
     </div>
 </template>
@@ -15,18 +25,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .my-container {
-        height: 100vh;
+    .jumbo-container {
         width: 100%;
         color: var(--tx-white-1);
-        background-image: url('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
-        background-size: contain;
-        background-repeat: no-repeat;
-        background-position: right;
         background-color: var(--bg-dark-3);
-        .description-container {
-            width: 30%;
+        .jumbo-description {
+            width: 35%;
+            .description-container {
+                width: 20%;
+                position: absolute;
+                top: 50%;
+                right: 60%;
+                font-size: larger;
+            }
         }
+        .jumbo-img {
+            width: 70%;
+            img {
+                max-width: 100%;
+            }
+        }
+        // background-image: url('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
+        // background-size: contain;
+        // background-repeat: no-repeat;
+        // background-position: right;
+        
+        // .description-container {
+        //     width: 30%;
+        // }
     }
     .fas.fa-play{
         color: var(--tx-white-1);
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="d-flex align-items-center">
+  <main>
       <Jumbo />
   </main>
 </template>
