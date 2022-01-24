5c198ef5a9173edb76888c0c0d31f966996c3a04
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-24 18:19:04 +0100

    add consultants component (to improve)

---
+++ src/components/Consultants.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="my-container d-flex align-items-center justify-content-space-around">
+    <div class="consultants-container">
+      <h4 class="consultants-title">OUR CONSULTANTS CAN HELP YOU <span></span></h4>
+      <div class="row row-cols-2">
+        <div v-for="(element, index) in consultantsArray" :key="index" class="col d-flex align-items-start justify-content-around p-3">
+          <div class="icon-container h-100 me-3">
+            <i :class="element.icon"></i>
+          </div>
+          <div class="description-container  h-100">
+            <h5 class="w-100">{{element.title}}</h5>
+            <p class="w-50">{{element.description}}</p>
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Consultants',
+    props: [
+      'consultantsArray',
+    ]
+}
+</script>
+
+<style lang="scss" scoped>
+  .my-container {
+    width: 100%;
+    height: 100vh;
+    .consultants-container{
+      height: 10%;
+      margin: 0 auto;
+      width: 80%;
+      height: 80%;
+      .consultants-title span {
+        display: inline-block;
+        width: 60%;
+        height: 2px;
+        background-color: var(--bg-green-1);
+      }
+      .row {
+        height: 90%;
+        .icon-container {
+          font-size: 1.5em;
+          i {
+            padding: 0.4em;
+            border: 1px solid var(--tx-white-1);
+            border-radius: 50%;
+            color: var(--bg-white-1);
+            background-color: var(--bg-red-1);
+          }
+        }
+      }
+    }
+  }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,45 @@
 <template>
   <main>
-      <Jumbo />
+      <Jumbo :icon="icon" />
+      <Consultants :consultantsArray="consultantsArray"/>
   </main>
 </template>
 
 <script>
 import Jumbo from './Jumbo.vue'
+import Consultants from './Consultants.vue'
 
 export default {
     name: "Main",
+    data(){
+        return {
+            consultantsArray: {
+                0: {
+                    icon: 'fas fa-chart-line',
+                    title: 'STATISTICAL CONSULTING',
+                    description:'When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a'
+                },
+                1: {
+                    icon: 'fas fa-bullhorn',
+                    title: 'DIGITAL CONSULTING',
+                    description:'When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a'
+                },
+                2: {
+                    icon: 'fas fa-wallet',
+                    title: 'BANKING CONSULTING',
+                    description:'When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a'
+                },
+                3: {
+                    icon: 'fas fa-bullseye',
+                    title: 'ENTERPRISE CONSULTING',
+                    description:'When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a'
+                }
+            }
+        }
+    },
     components: { 
         Jumbo,
+        Consultants,
     }
 }
 </script>
