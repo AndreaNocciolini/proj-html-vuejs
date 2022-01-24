07ffb93b53b4af9f918e8b057f1e76a391c43613
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-24 16:01:02 +0100

    add component jumbo

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="my-container d-flex">
+        <div class="description-container d-flex flex-column justify-content-center align-items-start ms-5">
+            <p> Always the best way you need it</p>
+            <h1>THE BEST BUSINESS CONSULTING</h1>
+            <div><button>READ MORE</button><i class="fas fa-play"></i></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Jumbo'
+}
+</script>
+
+<style lang="scss" scoped>
+    .my-container {
+        height: 100vh;
+        width: 100%;
+        background-image: url('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: right;
+        background-color: #22213d;
+        .description-container {
+            width: 30%;
+        }
+    }
+    .fas.fa-play{
+        color: white;
+        font-size: 0.5em;
+        border: 1px solid white;
+        padding: 1.5em;
+        border-radius: 50%;
+    }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,41 +1,20 @@
-// RESTART FROM HERE (USE COLOR ROOTS, FINISH FIRST PART MAIN)
-///////////////////////////////////////////////////////////////
 <template>
   <main class="d-flex align-items-center">
-      <div class="my-container d-flex">
-        <div class="description-container d-flex flex-column justify-content-center align-items-start ms-5">
-            <p> Always the best way you need it</p>
-            <h1>THE BEST BUSINESS CONSULTING</h1>
-            <div><button>READ MORE</button><i class="fas fa-play"></i></div>
-        </div>
-      </div>
+      <Jumbo />
   </main>
 </template>
 
 <script>
+import Jumbo from './Jumbo.vue'
+
 export default {
     name: "Main",
+    components: { 
+        Jumbo,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    .my-container {
-        height: 100vh;
-        width: 100%;
-        background-image: url('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
-        background-size: contain;
-        background-repeat: no-repeat;
-        background-position: right;
-        background-color: #22213d;
-        .description-container {
-            width: 30%;
-        }
-    }
-    .fas.fa-play{
-        color: white;
-        font-size: 0.5em;
-        border: 1px solid white;
-        padding: 1.5em;
-        border-radius: 50%;
-    }
+    
 </style>
